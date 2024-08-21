2d80e6134ec15e131476c24791cc3257829fd104
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2024-08-21 11:50:33 -0300

    Actualiza List Data

--- index.php
+++ index.php
@@ -15,37 +15,27 @@ include("heather.php");
 </header>
 
 <main>
-    <div class="container">
-    <div class="table_area table-responsive">
-       <table class="table table-striped table-hover">
-           <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Description</th>  
-                <th scope="col">#</th> 
-            </tr>
-           </thead>
-           <tbody>
-               <?php
-               while($fila=mysqli_fetch_assoc($query)){
-                ?>
-                   <tr class="cj-trDataList">
-                       <!-- <td><?php echo $fila["id"] ?></td> -->
-                       <th scope="row" class="cj-thDataName"><?php echo $fila["name"] ?></th>
-                       <td><div class="cj-tdDescriptionContent"><?php echo $fila["description"] ?></div></td>
-                       <!-- <td class="cj-tdDescriptionContent"><?php echo $fila["description"] ?></td> -->
-                       <td class="cj-tdMethod">
-                        <div class="cj-groupBtn">
-                            <a title="Delette" href="deletteData.php?id=<?php echo $fila["id"]?>"><i class="bi bi-trash3"></i></a>
-                            <a title="Edit" href="formEditarData.php?id=<?php echo $fila["id"]?>"><i class="bi bi-file-earmark-font"></i></a> 
-                            <a title="Print" href="viewData.php?id=<?php echo $fila["id"]?>"><i class="bi bi-card-checklist"></i></a> 
-                        </div>
-                        </td>
-                   </tr>
-              <?php } ?>                             
-           </tbody>
-        </table>
-    </div>
+     
+    <div class="container cj-table">
+        <div class="row">
+            <div class="col">Name</div>
+            <div class="col-6">Description</div>
+            <div class="col">Action</div>
+        </div>
+        <?php
+            while($fila=mysqli_fetch_assoc($query)){
+        ?>
+        <div class="row cj-list-row">
+            <div hidden><?php echo $fila["id"] ?></div>
+            <div class="col"> <?php echo $fila["name"] ?> </div>
+            <div class="col-6"> <?php echo $fila["description"] ?> </div>
+            <div class="col cj-groupBtn">
+                <a title="Delette" href="deletteData.php?id=<?php echo $fila["id"]?>"><i class="bi bi-trash3"></i></a>
+                <a title="Edit" href="formEditarData.php?id=<?php echo $fila["id"]?>"><i class="bi bi-file-earmark-font"></i></a> 
+                <a title="Print" href="viewData.php?id=<?php echo $fila["id"]?>"><i class="bi bi-card-checklist"></i></a> 
+            </div>
+        </div>
+        <?php } ?>  
     </div>
 
     <div>
